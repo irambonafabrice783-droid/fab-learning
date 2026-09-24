5c084f1771ce196b2fb23af1ea58c8b2430dc2b9
irambonafabrice783-droid <irambonafabrice783@gmail.com>
2026-09-24 16:32:00 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function startLearning() {
+    alert("Welcome to Fab-Learning! Let's start learning.");
+}
+
+function openSubjects() {
+    alert("Subjects section coming next!");
+}
+
+function openNotes() {
+    alert("Full Notes section coming next!");
+}
+
+function openExercises() {
+    alert("Exercises section coming next!");
+}
+
+function openQuizzes() {
+    alert("Quizzes section coming next!");
+}
+
+function openExams() {
+    alert("Timed Exams section coming next!");
+}
+
+function askAI() {
+    alert("Ask AI will be connected later.");
+}
+
+function openReport() {
+    alert("Your Report Card will appear here.");
+}
